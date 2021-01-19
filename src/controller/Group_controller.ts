bc253eecb58b036f6512fc17b3149c96b7719c80
Nieto Clement <clement.nieto@epitech.eu>
2021-01-19 13:55:09 +0100

    Add basic controller

---
+++ src/controller/Group_controller.ts
@@ -0,0 +1,34 @@
+import { Group } from "../model/Group"
+
+export class Group_controller
+{
+    public group_list:Array<Group>;
+    private nb_group = 0;
+    constructor() {
+        this.group_list = new Array();
+    }
+    create_group(creator:string, name:string) {
+        console.log("Creation du groupe...");
+        this.group_list[this.nb_group] = new Group(creator, name);
+        this.nb_group++;
+    }
+    add_group(group:Group) {
+        this.group_list[this.nb_group] = group;
+        this.nb_group++;
+    }
+    get_joined_groups(name:string) {
+        var result = new Array();
+        var id = -1;
+        var i = 0;
+
+        this.group_list.forEach(item => {
+            id = item.get_user_id_from_name(name);
+            if (id != -1) {
+                console.log(name + " fait parti du groupe " + item.name);
+                result[i] = item;
+                i++;
+            }
+        });
+        return result;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,4 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { Group } from "./model/Group"
+import { Group_controller } from "./controller/Group_controller"
 
- import { Group } from "./model/Group"
-
-var test = new Group("A", "Test_group");
-
-test.addUser("B");
-test.addExpense("A", "2 pizzas", 20);
-test.addExpense("B", "1 pack de bieres", 7);
-test.addUser("C");
-test.addUser("D");
-test.deleteExpense(1);
-test.addExpense("B", "2 packs de bieres", 14);
-test.addExpense("C", "Verres a shot", 5.99);
-test.addUser("E");
-test.addUser("F");
-test.addExpense("F", "1 bouteille de vodka", 14.99);
-test.addExpense("E", "3 bouteille de diluant", 8);
-test.settle_group_balance();
+var group_controller = new Group_controller();
